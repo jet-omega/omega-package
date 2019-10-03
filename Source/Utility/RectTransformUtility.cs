76ab5e5fb404e14bf48e524ed2d90b103dbc3712
Sergey Davydov (sergey-cs) <prozerox2@yandex.ru>
2019-10-03 11:45:04 +0300

    create RectTransfromTools

---
+++ Source/Utility/RectTransformUtility.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+
+namespace Omega.Tools
+{
+    public static class RectTransformUtility
+    {
+        /// <summary>
+        /// Возвращает всех потомков указанного трансформа, если потомок этого трансформа не кастится к RectTransfrom,
+        /// то он не будет добавлен в конечный массив  
+        /// </summary>
+        /// <param name="root">Трансформ, относительно которого будет осуществляться поиск потомков</param>
+        /// <returns>Массив потомков, кроме тех которые не являются производными от RectTransform</returns>
+        /// <exception cref="ArgumentNullException">Параметр <param name="root"/>>указывает на null</exception>
+        /// <exception cref="MissingReferenceException">Параметр <param name="root"/>>указывает на уничтоженный объект</exception>
+        [NotNull]
+        public static RectTransform[] GetChildes([NotNull] RectTransform root)
+        {
+            if (ReferenceEquals(root, null))
+                throw new ArgumentNullException(nameof(root));
+            if (!root)
+                throw new MissingReferenceException(nameof(root));
+
+            return GetChildesWithoutChecks(root);
+        }
+
+        [NotNull]
+        internal static RectTransform[] GetChildesWithoutChecks([NotNull] RectTransform rectTransform)
+        {
+            var transformChildes = TransformUtility.GetChildesWithoutChecks(rectTransform);
+
+            var rectTransformChildesList = new List<RectTransform>(transformChildes.Length);
+
+            foreach (var child in transformChildes)
+                if (child is RectTransform rectTransformChild)
+                    rectTransformChildesList.Add(rectTransformChild);
+
+            var rectTransformChildes = rectTransformChildesList.ToArray();
+
+            return rectTransformChildes;
+        }
+    }
+}
---
+++ Tests/RectTransfromUtilityTests.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace Omega.Tools.Tests
+{
+    public class RectTransformUtilityTests
+    {
+        [Test]
+        public void GetChildesShouldReturnArrayWithTwoElementsWhenEightChildesIsRectAndTenChildesIsTransformTest()
+        {
+            //Test params
+            const int countChildesWithRectTransform = 8;
+            const int countChildesWithTransform = 10;
+
+            var gameObjectInstance = new GameObject("GetChildesShouldReturnArrayWithTwoElementsTest");
+            gameObjectInstance.AddComponent<RectTransform>();
+
+            foreach (var num in Enumerable.Range(0, countChildesWithRectTransform))
+                gameObjectInstance.Attach(new GameObject("Test " + num)).AddComponent<RectTransform>();
+
+            foreach (var num in Enumerable.Range(countChildesWithRectTransform, countChildesWithTransform))
+                gameObjectInstance.Attach(new GameObject("Test " + num));
+
+            var childes = RectTransformUtility.GetChildes(gameObjectInstance.GetComponent<RectTransform>());
+
+            Assert.AreEqual(childes.Length, countChildesWithRectTransform);
+
+            Object.Destroy(gameObjectInstance);
+        }
+
+        [Test]
+        public void GetChildesShouldReturnEmptyArrayWhenChildesWereDestroyedTest()
+        {
+            const int countChildes = 3;
+
+            var gameObjectInstance =
+                new GameObject(nameof(GetChildesShouldReturnEmptyArrayWhenChildesWereDestroyedTest));
+
+            gameObjectInstance.AddComponent<RectTransform>();
+
+            foreach (var num in Enumerable.Range(0, countChildes))
+                gameObjectInstance.Attach(new GameObject("Test " + num)).AddComponent<RectTransform>();
+
+            gameObjectInstance.GetChildes().Select(e => e.gameObject).ToList().ForEach(Object.DestroyImmediate);
+
+            var childes = RectTransformUtility.GetChildes(gameObjectInstance.GetComponent<RectTransform>());
+
+            Assert.Zero(childes.Length);
+        }
+
+        [Test]
+        public void GetChildesShouldThrowMissingReferenceExceptionWhenParameterIsDestroyedGameObjectTest()
+        {
+            var gameObjectInstance =
+                new GameObject(
+                    nameof(GetChildesShouldThrowMissingReferenceExceptionWhenParameterIsDestroyedGameObjectTest));
+
+            var rectTransform = gameObjectInstance.AddComponent<RectTransform>();
+
+            Object.DestroyImmediate(gameObjectInstance);
+
+            Assert.Throws<MissingReferenceException>(() => RectTransformUtility.GetChildes(rectTransform));
+        }
+        
+        [Test]
+        public void GetChildesShouldThrowArgumentNullExceptionWhenParameterIsNullTest()
+        {
+            Assert.Throws<ArgumentNullException>(() =>
+                // ReSharper disable once AssignNullToNotNullAttribute
+                RectTransformUtility.GetChildes(null));
+        }
+    }
+}
