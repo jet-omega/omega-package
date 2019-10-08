13de69da4a33eacbbd616e152cc8690015123ae6
sergey-cs <prozerox2@yandex.ru>
2019-10-09 00:05:23 +0300

    add GetGenericArgumentsOfDefinitionInterface and ContainsInterfaceDefinition methods in ReflectionUtility, create tests for ReflectionUtility

--- Source/Utility/ReflectionUtility.cs
+++ Source/Utility/ReflectionUtility.cs
@@ -11,19 +11,65 @@ namespace Omega.Tools
         public static MethodInfo[] GetMethodsWithAttribute<T>([NotNull] Type type)
             where T : Attribute
         {
-            if(type == null)
+            if (type == null)
                 throw new ArgumentNullException(nameof(type));
 
             return GetMethodsWithAttributeWithoutChecks<T>(type, null).ToArray();
         }
-        
+
         public static MethodInfo[] GetMethodsWithAttribute<T>([NotNull] Type type, [CanBeNull] Func<T, bool> selector)
             where T : Attribute
         {
-            if(type == null)
+            if (type == null)
                 throw new ArgumentNullException(nameof(type));
 
             return GetMethodsWithAttributeWithoutChecks<T>(type, selector).ToArray();
+        }
+
+        public static bool ContainsInterfaceDefinitionInType(Type type, Type definitionType)
+        {
+            if(type == null)
+                throw new ArgumentNullException(nameof(type));
+            if(definitionType == null)
+                throw new ArgumentNullException(nameof(definitionType));
+            if(!definitionType.IsGenericTypeDefinition)
+                throw new ArgumentException(nameof(definitionType));
+
+            return ContainsInterfaceDefinitionInTypeWithoutChecks(type, definitionType);
+        }
+
+        public static Type[] GetGenericArgumentsOfDefinitionInterface(Type sourceType, Type definitionInterface)
+        {
+            if(sourceType == null)
+                throw new ArgumentNullException(nameof(sourceType));
+            if(definitionInterface == null)
+                throw new ArgumentNullException(nameof(definitionInterface));
+            
+            if(!definitionInterface.IsInterface)
+                throw new ArgumentException(nameof(definitionInterface));
+            if(!definitionInterface.IsGenericTypeDefinition)
+                throw new ArgumentException(nameof(definitionInterface));
+
+            if (!ContainsInterfaceDefinitionInTypeWithoutChecks(sourceType, definitionInterface))
+                throw new ArgumentException();
+
+            return GetGenericArgumentsOfDefinitionInterfaceWithoutChecks(sourceType, definitionInterface);
+        }
+        
+        internal static Type[] GetGenericArgumentsOfDefinitionInterfaceWithoutChecks(Type sourceType, Type definitionInterface)
+        {
+            return sourceType.GetInterfaces()
+                .Where(_ => _.IsGenericType)
+                .First(_ => _.GetGenericTypeDefinition() == definitionInterface)
+                .GetGenericArguments();
+        }
+        
+        internal static bool ContainsInterfaceDefinitionInTypeWithoutChecks(Type type, Type definitionType)
+        {
+            return type.GetInterfaces()
+                    .Where(_ => _.IsGenericType)
+                    .Select(_ => _.GetGenericTypeDefinition())
+                    .Any(_ => definitionType == _);
         }
 
         [NotNull]
