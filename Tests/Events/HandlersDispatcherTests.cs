82dd9bf89fa99a8e5a5feb8c0b921e0f04046a6c
sergey-cs <prozerox2@yandex.ru>
2019-10-08 17:14:32 +0300

    rework unit-tests, fix bugs

--- Tests/Events/HandlersDispatcherTests.cs
+++ Tests/Events/HandlersDispatcherTests.cs
@@ -8,29 +8,29 @@ namespace Omega.Tools.Experimental.Events.Tests
 {
     public class HandlersDispatcherTests
     {
-        [Test]
-        public void HandlersDispatcherProviderShouldReturnDefaultProvider()
-        {
-            var provider = HandlersDispatcher<HandlersDispatcherTestsEvent>.Provider;
-            Assert.AreEqual(provider.GetType(), new DefaultHandlersProvider<HandlersDispatcherTestsEvent>().GetType());
-            
-            HandlersDispatcher<HandlersDispatcherTestsEvent>.SetProvider(null);
-        }
-        
-        [Test]
-        public void HandlersDispatcherSetProviderShouldSetupCustomProvider()
-        {
-            var provider = new DefaultHandlersProvider<HandlersDispatcherTestsEvent>();
-            HandlersDispatcher<HandlersDispatcherTestsEvent>.SetProvider(provider);
-            
-            Assert.AreEqual(HandlersDispatcher<HandlersDispatcherTestsEvent>.Provider, provider);
-            
-            HandlersDispatcher<HandlersDispatcherTestsEvent>.SetProvider(null);
-        }
-
-        private struct HandlersDispatcherTestsEvent
-        {
-            public object someArg;
-        }
+//        [Test]
+//        public void HandlersDispatcherProviderShouldReturnDefaultProvider()
+//        {
+//            var provider = HandlersDispatcher<HandlersDispatcherTestsEvent>.Provider;
+//            Assert.AreEqual(provider.GetType(), new DefaultHandlersProvider<HandlersDispatcherTestsEvent>().GetType());
+//            
+//            HandlersDispatcher<HandlersDispatcherTestsEvent>.SetProvider(null);
+//        }
+//        
+//        [Test]
+//        public void HandlersDispatcherSetProviderShouldSetupCustomProvider()
+//        {
+//            var provider = new DefaultHandlersProvider<HandlersDispatcherTestsEvent>();
+//            HandlersDispatcher<HandlersDispatcherTestsEvent>.SetProvider(provider);
+//            
+//            Assert.AreEqual(HandlersDispatcher<HandlersDispatcherTestsEvent>.Provider, provider);
+//            
+//            HandlersDispatcher<HandlersDispatcherTestsEvent>.SetProvider(null);
+//        }
+//
+//        private struct HandlersDispatcherTestsEvent
+//        {
+//            public object someArg;
+//        }
     }
 }
