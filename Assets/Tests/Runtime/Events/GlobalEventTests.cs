5c75153ae749e730ba0c164595fd494424278dec
sergey-cs <prozerox2@yandex.ru>
2019-10-13 12:53:51 +0300

    create runtime unit-tests assembly, omega.events unit-tests is runtime tests

--- Assets/Tests/Runtime/Events/GlobalEventTests.cs
+++ Assets/Tests/Runtime/Events/GlobalEventTests.cs
@@ -24,19 +24,25 @@ namespace Omega.Tools.Experimental.Events.Tests
         {
             int handlerInvocationCount = 0;
 
-            var initialScene = EditorSceneManager.NewScene(NewSceneSetup.EmptyScene, NewSceneMode.Single);
-            initialScene.name = $"Initial scene for {nameof(EventManagerDispatcherShouldNotChangeEventManagerWhenSceneWereSwitchedTest)}";
-            
+            var initialScene = SceneManager.CreateScene("initial",
+                new CreateSceneParameters {localPhysicsMode = LocalPhysicsMode.None});
+            initialScene.name =
+                $"Initial scene for {nameof(EventManagerDispatcherShouldNotChangeEventManagerWhenSceneWereSwitchedTest)}";
+            SceneManager.SetActiveScene(initialScene);
+
             EventAggregator.AddHandler<TestGlobalWithAttributeEvent>(_ => handlerInvocationCount++);
             EventAggregator.Event<TestGlobalWithAttributeEvent>(default);
-            
+
             Assert.AreEqual(1, handlerInvocationCount);
 
-            var secondScene = EditorSceneManager.NewScene(NewSceneSetup.EmptyScene, NewSceneMode.Single);
-            secondScene.name = $"Second scene for {nameof(EventManagerDispatcherShouldNotChangeEventManagerWhenSceneWereSwitchedTest)}";
-            
+            var secondScene = SceneManager.CreateScene("second",
+                new CreateSceneParameters {localPhysicsMode = LocalPhysicsMode.None});
+            secondScene.name =
+                $"Second scene for {nameof(EventManagerDispatcherShouldNotChangeEventManagerWhenSceneWereSwitchedTest)}";
+            SceneManager.SetActiveScene(secondScene);
+
             EventAggregator.Event<TestGlobalWithAttributeEvent>(default);
-            
+
             Assert.AreEqual(2, handlerInvocationCount);
         }
 
