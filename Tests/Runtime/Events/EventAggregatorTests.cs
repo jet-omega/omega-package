bf8f26d3fbc9fa4fdab208dedc8c1ef3a16901ef
sergey-cs <prozerox2@yandex.ru>
2019-10-13 21:56:54 +0300

    create unity hanlder adapter, tests ans support UnityEngine.Object handlers

---
+++ Tests/Runtime/Events/EventAggregatorTests.cs
@@ -0,0 +1,61 @@
+using System;
+using NUnit.Framework;
+using Omega.Tools.Experimental.Events.Internals;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace Omega.Tools.Experimental.Events.Tests
+{
+    public class EventAggregatorTests
+    {
+        [Test]
+        public void AddHandlerShouldAddUnityObjectHandlerTest()
+        {
+            bool flag = false;
+
+            var gameObject = new GameObject(nameof(AddHandlerShouldAddUnityObjectHandlerTest));
+            var handler = gameObject.AddComponent<TestMonoBehaviour>();
+            handler.callback = () => flag = true;
+            
+            EventAggregator.AddHandler(handler);
+            EventAggregator.Event(new TestEvent());
+
+            Assert.True(flag);
+            
+            Object.DestroyImmediate(gameObject);
+        }
+        
+        [Test]
+        public void AddHandlerShouldRemoveUnityObjectHandlerTest()
+        {
+            var gameObject = new GameObject(nameof(AddHandlerShouldRemoveUnityObjectHandlerTest));
+            var handler = gameObject.AddComponent<TestMonoBehaviour>();
+            handler.callback = () => Assert.Fail();
+            
+            EventAggregator.AddHandler(handler);
+            EventAggregator.RemoveHandler(handler);
+            EventAggregator.Event(new TestEvent());
+
+            Object.DestroyImmediate(gameObject);
+        }
+
+        [SetUp]
+        [TearDown]
+        public void ClearHandlers()
+            => EventManagerDispatcher<TestEvent>.RemoveEventManagerInternal();
+        
+        private struct TestEvent
+        {
+        }
+
+        private class TestMonoBehaviour : MonoBehaviour, IEventHandler<TestEvent>
+        {
+            public Action callback;
+
+            public void Execute(TestEvent arg)
+            {
+                callback.Invoke();
+            }
+        }
+    }
+}
