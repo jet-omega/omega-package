41662d110f78a8d333a816bc824a15e24b485e6a
sergey-cs <prozerox2@yandex.ru>
2019-10-13 12:55:37 +0300

    event aggreagator throws platfrom not supported exception when his api using in edit-mode

--- Source/Experimental/Event/EventAggregator.cs
+++ Source/Experimental/Event/EventAggregator.cs
@@ -2,30 +2,64 @@ using System;
 using System.Collections;
 using Omega.Tools.Experimental.Event;
 using Omega.Tools.Experimental.Events.Internals;
+using UnityEngine;
 
 namespace Omega.Tools.Experimental.Events
 {
     public static class EventAggregator
     {
         public static void Event<TEvent>(TEvent arg)
-            => EventManagerDispatcher<TEvent>.GetEventManager().Event(arg);
+        {
+#if UNITY_EDITOR
+            if (!Application.isPlaying)
+                throw new PlatformNotSupportedException();
+#endif
+            EventManagerDispatcher<TEvent>.GetEventManager().Event(arg);
+        }
 
         public static IEnumerator EventAsync<TEvent>(TEvent arg)
-            => EventManagerDispatcher<TEvent>.GetEventManager().EventAsync(arg);
-        
+        {
+#if UNITY_EDITOR
+            if (!Application.isPlaying) 
+                throw new PlatformNotSupportedException();
+#endif
+            return EventManagerDispatcher<TEvent>.GetEventManager().EventAsync(arg);
+        }
+
         public static void AddHandler<TEvent>(IEventHandler<TEvent> handler)
-            => EventManagerDispatcher<TEvent>.GetEventManager().AddHandler(handler);
+        {
+#if UNITY_EDITOR
+            if (!Application.isPlaying)
+                throw new PlatformNotSupportedException();
+#endif
+            EventManagerDispatcher<TEvent>.GetEventManager().AddHandler(handler);
+        }
+
         public static void RemoveHandler<TEvent>(IEventHandler<TEvent> handler)
-            => EventManagerDispatcher<TEvent>.GetEventManager().RemoveHandler(handler);
-        
+        {
+#if UNITY_EDITOR
+            if (!Application.isPlaying)
+                throw new PlatformNotSupportedException();
+#endif
+            EventManagerDispatcher<TEvent>.GetEventManager().RemoveHandler(handler);
+        }
+
         public static void AddHandler<TEvent>(Action<TEvent> handler)
         {
+#if UNITY_EDITOR
+            if (!Application.isPlaying)
+                throw new PlatformNotSupportedException();
+#endif
             var actionHandlerAdapter = ActionHandlerAdapterBuilder.Build(handler);
             EventManagerDispatcher<TEvent>.GetEventManager().AddHandler(actionHandlerAdapter);
         }
 
         public static void RemoveHandler<TEvent>(Action<TEvent> handler)
         {
+#if UNITY_EDITOR
+            if (!Application.isPlaying)
+                throw new PlatformNotSupportedException();
+#endif
             var actionHandlerAdapter = ActionHandlerAdapterBuilder.Build(handler);
             EventManagerDispatcher<TEvent>.GetEventManager().RemoveHandler(actionHandlerAdapter);
         }
