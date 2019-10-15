185d5d8deffef1e8ad06ab2d98d49de7cd07dae0
Sergey Davydov (sergey-cs) <prozerox2@yandex.ru>
2019-10-15 14:45:29 +0300

    delete GlobalEvent and SceneEvent attribute, create EventCoverage

---
+++ Source/Experimental/Event/EventCoverageAttribute.cs
@@ -0,0 +1,16 @@
+using System;
+using Omega.Tools.Experimental.Event;
+
+namespace Omega.Tools.Experimental.Events.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct)]
+    public class EventCoverageAttribute : Attribute
+    {
+        public readonly EventCoverage Coverage;
+
+        public EventCoverageAttribute(EventCoverage coverage)
+        {
+            Coverage = coverage;
+        }
+    }
+}
