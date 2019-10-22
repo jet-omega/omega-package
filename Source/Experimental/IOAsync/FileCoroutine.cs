81bd2b66c31b1b6b5b47b24b8ce87ba5378e610f
Sergey Davydov (sergey-cs) <prozerox2@yandex.ru>
2019-10-22 15:04:40 +0300

    create io routines

---
+++ Source/Experimental/IOAsync/FileCoroutine.cs
@@ -0,0 +1,19 @@
+using System.IO;
+using Omega.Experimental.Routines;
+
+namespace Omega.Experimental.Routines.IO
+{
+    public static class FileRoutine
+    {
+        public static Routine WriteAllTextRoutine(string path, string text)
+            => Routine.Task(() => File.WriteAllText(path, text));
+
+        public static Routine<string> ReadAllTextRoutine(string path)
+            => Routine.Task(() => File.ReadAllText(path));
+
+        public static Routine WriteAllBytesRoutine(string path, byte[] data) =>
+            Routine.Task(() => File.WriteAllBytes(path, data));
+        public static Routine<byte[]> ReadAllBytesRoutine(string path) =>
+            Routine.Task(() => File.ReadAllBytes(path));
+    }
+}
