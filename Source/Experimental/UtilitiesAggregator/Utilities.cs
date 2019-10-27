184097f8a0bb68785d9fd307bd9bf846f055c9bf
sergey-cs <prozerox2@yandex.ru>
2019-10-27 17:49:07 +0300

    add utils for time

--- Source/Experimental/UtilitiesAggregator/Utilities.cs
+++ Source/Experimental/UtilitiesAggregator/Utilities.cs
@@ -7,9 +7,11 @@ namespace Omega.Experimental
         private static TransfromUtilites _transfrom = new TransfromUtilites();
         private static GameObjectUtilities _gameObject = new GameObjectUtilities();
         private static RectTransformUtilities _rectTransform = new RectTransformUtilities();
+        private static TimeUtilities _time = new TimeUtilities();
 
         public static TransfromUtilites Transfrom => _transfrom;
         public static GameObjectUtilities GameObject => _gameObject;
         public static RectTransformUtilities RectTransform => _rectTransform;
+        public static TimeUtilities Time => _time;
     }
 }
