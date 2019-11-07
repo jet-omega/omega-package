81fe1425dda6382b65ba5847507a00ad796981e7
Sergey Davydov (sergey-cs) <prozerox2@yandex.ru>
2019-11-07 19:05:50 +0300

    add attributes

--- Assets/Source/AssemblyAttributes.cs
+++ Assets/Source/AssemblyAttributes.cs
@@ -1,4 +1,19 @@
+using System.Reflection;
 using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("Omega.Tools.Editor.Tests")]
 [assembly: InternalsVisibleTo("Omega.Tools.Runtime.Tests")]
+
+// General information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle( "OmegaPackage" )]
+[assembly: AssemblyDescription( "Tools for Unity" )]
+[assembly: AssemblyCompany( "Ltd. ProfIt" )]
+
+// Version information for an assembly consists of the following four values:
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+[assembly: AssemblyVersion( "0.8.0" )]
