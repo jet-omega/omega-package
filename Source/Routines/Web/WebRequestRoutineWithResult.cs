304e95ffbd4d9f77c2d7791715442c3d1f79150f
sergey-cs <prozerox2@yandex.ru>
2019-11-24 20:34:18 +0300

    web request routine support progress provider

--- Source/Routines/Web/WebRequestRoutineWithResult.cs
+++ Source/Routines/Web/WebRequestRoutineWithResult.cs
@@ -1,14 +1,16 @@
 using System;
 using System.Collections;
 using System.Net.Http;
+using UnityEngine;
 using UnityEngine.Networking;
 
 namespace Omega.Routines.Web
 {
-    public abstract class WebRequestRoutine<T> : Routine<T>
+    public abstract class WebRequestRoutine<T> : Routine<T>, IProgressRoutineProvider
     {
         public readonly UnityWebRequest WebRequest;
         private Func<DownloadHandler, T> _resultProvider;
+        private AsyncOperation _webRequestAsyncOperation;
 
         public WebRequestRoutine(UnityWebRequest webRequest, Func<DownloadHandler, T> resultProvider)
         {
@@ -18,7 +20,7 @@ namespace Omega.Routines.Web
 
         protected override IEnumerator RoutineUpdate()
         {
-            yield return WebRequest.SendWebRequest();
+            yield return WebRequest.SendWebRequest().GetSelf(out _webRequestAsyncOperation);
             if (WebRequest.isNetworkError || WebRequest.isHttpError)
                 throw new HttpRequestException(WebRequestRoutine.GetErrorMessage(WebRequest));
 
@@ -26,6 +28,11 @@ namespace Omega.Routines.Web
             
             var result = downloadHandler != null ? _resultProvider(WebRequest.downloadHandler) : default;
             SetResult(result);
+        }
+
+        public float GetProgress()
+        {
+            return _webRequestAsyncOperation?.progress ?? 0;
         }
     }
 }
