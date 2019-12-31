e1ed404d17adc97dd5eb8f332d48a0c2d860b218
sergey-cs <prozerox2@yandex.ru>
2019-12-31 19:07:25 +0300

    ListPool improvements

--- Source/ListPool.cs
+++ Source/ListPool.cs
@@ -6,21 +6,23 @@ namespace Omega.Package
 {
     public static class ListPool<T>
     {
-        public const int DefaultListCapacity = 20;
+        public const int DefaultListCapacity = 10;
         [NotNull] private static readonly Stack<List<T>> Pool = new Stack<List<T>>(2);
 
+        public static int PoolSize => Pool.Count;
+        
         [NotNull]
         public static List<T> Rent()
         {
-            return Pool.Count == 0
+            return Pool.Count is 0
                 ? new List<T>(DefaultListCapacity)
                 : Pool.Pop();
         }
         public static List<T> Rent(int requireCapacity)
         {
-            if (Pool.Count == 0)
+            if (Pool.Count is 0)
             {
-                var bestCapacity = Math.Max(DefaultListCapacity, requireCapacity);
+                var bestCapacity = Math.Max(requireCapacity, DefaultListCapacity);
                 return new List<T>(bestCapacity);
             }
 
@@ -31,14 +33,44 @@ namespace Omega.Package
             return list;
         }
 
+        public static ListPoolHandler<T> Get(out List<T> list)
+        {
+            list = Rent();
+            return new ListPoolHandler<T>(list);
+        }
+        
+        [Obsolete("Use Return method")]
         public static void Push([NotNull] List<T> list)
+        {
+            Return(list);
+        }
+
+        public static void Flush()
+        {
+            Pool.Clear();
+            Pool.TrimExcess();
+        }
+        
+        
+        public static void Return([NotNull] List<T> list)
         {
             if(list is null)
                 throw new ArgumentNullException(nameof(list));
             
-            PushInternal(list);
+            ReturnInternal(list);
         }
-        internal static void PushInternal(List<T> list)
+
+        public static T[] ReturnToArray([NotNull] List<T> list)
+        {
+            if(list is null)
+                throw new ArgumentNullException(nameof(list));
+
+            var array = list.ToArray();
+            ReturnInternal(list);
+            return array;
+        }
+
+        internal static void ReturnInternal(List<T> list)
         {
             list.Clear();
             Pool.Push(list);
---
+++ Tests/Editor/ListPoolTests.cs
@@ -0,0 +1,38 @@
+using System.Collections.Generic;
+using NUnit.Framework;
+using Omega.Package;
+
+namespace Omega.Tools.Tests.Editor
+{
+    public class ListPoolTests
+    {
+        [Test]
+        public void GetMethodShouldRentListFromPool()
+        {
+            ListPool<float>.Return(new List<float>(ListPool<float>.DefaultListCapacity));
+
+            ListPool<float>.Get(out var list);
+
+            Assert.Zero(ListPool<float>.PoolSize);
+        }
+
+        [Test]
+        public void ReturnMethodShouldPushListToPool()
+        {
+            ListPool<float>.Return(new List<float>(10));
+            Assert.AreEqual(1, ListPool<float>.PoolSize);
+        }
+
+        [Test]
+        public void ListPoolHandlerShouldReturnListToPool()
+        {
+            var handler = ListPool<float>.Get(out var list);
+            handler.Dispose();
+            Assert.AreEqual(1, ListPool<float>.PoolSize);
+        }
+
+        [SetUp]
+        public void FlushListPoolOfFloat()
+            => ListPool<float>.Flush(); // clean pool
+    }
+}
