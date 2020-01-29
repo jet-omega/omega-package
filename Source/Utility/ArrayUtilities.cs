205dc436163cea80222159f277b9bb7979f2532a
David Nightingale <nightrider2552@gmail.com>
2020-01-29 18:21:01 +0300

    ArrayUtilities and Tests

---
+++ Source/Utility/ArrayUtilities.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Omega.Tools
+{
+    public class ArrayUtilities
+    {
+        /// <summary>
+        /// Добавить элемент item в конец массива array
+        /// </summary>
+        public void Add<T>(ref T[] array, T item)
+        {
+            Array.Resize(ref array, array.Length + 1);
+            array[array.Length - 1] = item;
+        }
+
+        /// <summary>
+        /// Добавить массив items в конец массива array
+        /// </summary>
+        public void AddRange<T>(ref T[] array, T[] items)
+        {
+            var sourceArrayLength = array.Length;
+            var newArrayLength = sourceArrayLength + items.Length;
+
+            Array.Resize(ref array, newArrayLength);
+
+            for (var i = 0; i < items.Length; i++)
+                array[i + sourceArrayLength] = items[i];
+        }
+
+        /// <summary>
+        /// Удалить первое вхождение item в массив array
+        /// </summary>
+        /// <returns>True если элемент был удален</returns>
+        public bool Remove<T>(ref T[] array, T item)
+        {
+            bool isRemoved = false;
+            for (var i = 0; i < array.Length - (isRemoved ? 1 : 0); ++i)
+            {
+                if (array[i].Equals(item))
+                    isRemoved = true;
+
+                if (isRemoved && i < array.Length - 1)
+                    array[i] = array[i + 1];
+            }
+
+            if (isRemoved)
+                Array.Resize(ref array, array.Length - 1);
+
+            return isRemoved;
+        }
+
+        /// <summary>
+        /// Удалить элемент массива array с индексом index
+        /// </summary>
+        public void RemoveAt<T>(ref T[] array, int index)
+        {
+            if (array.Length == 1 && index == 0)
+            {
+                Array.Resize(ref array, 0);
+                return;
+            }
+
+            if (index != array.Length - 1) // Если нужно удалить НЕ последний элемент, нужно сдвинуть все остальные
+            {
+                for (var i = index; i < array.Length - 1; i++)
+                    array[i] = array[i + 1];
+            }
+
+            Array.Resize(ref array, array.Length - 1);
+        }
+
+        /// <summary>
+        /// Удалить все вхождения элемента item в массив array
+        /// </summary>
+        ///<returns>Количество удаленных элементов</returns>
+        public int RemoveAll<T>(ref T[] array, T item)
+        {
+            int j = 0;
+            var startArrayLength = array.Length;
+
+            for (var i = 0; i < startArrayLength; ++i)
+            {
+                if (!array[i].Equals(item))
+                {
+                    array[j] = array[i];
+                    j++;
+                }
+            }
+
+            Array.Resize(ref array, j);
+            return startArrayLength - j;
+        }
+
+        /// <summary>
+        /// Вставить элемент item в массив array на индекс index
+        /// </summary>
+        public void Insert<T>(ref T[] array, int index, T item)
+        {
+            Array.Resize(ref array, array.Length + 1);
+
+            for (var i = array.Length - 1; i > index; i--) // array.Length здесь на 1 больше, чем исходный
+                array[i] = array[i - 1];
+
+            array[index] = item;
+        }
+
+        /// <summary>
+        /// Проверка массивов на равенство. Сравнение элементов по значениям
+        /// </summary>
+        public bool ArrayEquals<T>(T[] lhs, T[] rhs)
+        {
+            return lhs.SequenceEqual(rhs);
+        }
+
+        /// <summary>
+        /// Проверка массивов на равенство. Сравнение элементов по ссылкам
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="lhs"></param>
+        /// <param name="rhs"></param>
+        /// <returns></returns>
+        public bool ArrayReferenceEquals<T>(T[] lhs, T[] rhs)
+        {
+            if (lhs == null || rhs == null)
+                return lhs == rhs;
+            if (lhs.Length != rhs.Length)
+                return false;
+            for (var index = 0; index < lhs.Length; ++index)
+            {
+                if (!ReferenceEquals(lhs[index], rhs[index]))
+                    return false;
+            }
+            return true;
+        }
+
+        public T[] FindAll<T>(T[] array, Predicate<T> match)
+        {
+            return Array.FindAll(array, match);
+        }
+
+        public T Find<T>(T[] array, Predicate<T> match)
+        {
+            return Array.Find(array, match);
+        }
+
+        public int FindIndex<T>(T[] array, Predicate<T> match)
+        {
+            return Array.FindIndex(array, match);
+        }
+
+        public int IndexOf<T>(T[] array, T value)
+        {
+            return Array.IndexOf(array, value);
+        }
+
+        public int LastIndexOf<T>(T[] array, T value)
+        {
+            return Array.LastIndexOf(array, value);
+        }
+
+        public bool Contains<T>(T[] array, T item)
+        {
+            // ReSharper disable once ForCanBeConvertedToForeach
+            // ReSharper disable once LoopCanBeConvertedToQuery
+            for (var i = 0; i < array.Length; i++)
+            {
+                if (array[i].Equals(item))
+                    return true;
+            }
+
+            return false;
+        }
+
+        public void Clear<T>(ref T[] array)
+        {
+            Array.Clear(array, 0, array.Length);
+            Array.Resize(ref array, 0);
+        }
+    }
+}
---
+++ Tests/Editor/ArrayUtilitiesTests.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using UnityEditor.VersionControl;
+using Omega.Experimental;
+
+namespace Omega.Tools.Tests
+{
+    public sealed class ArrayUtilitiesTests
+    {
+        [Test]
+        public void AddTest()
+        {
+            var array = new[] { 1, 2, 3, 4 };
+            var item = 5;
+            var expected = new[] { 1, 2, 3, 4, 5 };
+
+            Utilities.Array.Add(ref array, item);
+            Assert.AreEqual(expected, array);
+
+            array = new int[0];
+            item = 1;
+            expected = new[] { 1 };
+
+            Utilities.Array.Add(ref array, item);
+            Assert.AreEqual(expected, array);
+        }
+
+        [Test]
+        public void AddRangeTest()
+        {
+            var array = new[] { 1, 2, 3, 4 };
+            var items = new[] { 5, 6, 7, 8 };
+            var expected = new[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+
+            Utilities.Array.AddRange(ref array, items);
+            Assert.AreEqual(expected, array);
+
+            array = new int[0];
+            items = new[] { 1, 2 };
+            expected = new[] { 1, 2 };
+
+            Utilities.Array.AddRange(ref array, items);
+            Assert.AreEqual(expected, array);
+
+            array = new[] { 1, 2 };
+            items = new int[0];
+            expected = new[] { 1, 2 };
+
+            Utilities.Array.AddRange(ref array, items);
+            Assert.AreEqual(expected, array);
+        }
+
+        [Test]
+        public void RemoveTest()
+        {
+            var array = new[] { 1, 2, 3, 4, 5 };
+            var item = 3;
+            var expected = new[] { 1, 2, 4, 5 };
+
+            Utilities.Array.Remove(ref array, item);
+            Assert.AreEqual(expected, array);
+
+            array = new[] { 1, 2, 3, 4, 5 };
+            item = 5;
+            expected = new[] { 1, 2, 3, 4 };
+
+            Utilities.Array.Remove(ref array, item);
+            Assert.AreEqual(expected, array);
+
+            array = new[] { 1, 2, 3, 4, 5 };
+            item = 1;
+            expected = new[] { 2, 3, 4, 5 };
+
+            Utilities.Array.Remove(ref array, item);
+            Assert.AreEqual(expected, array);
+
+            array = new[] { 1 };
+            item = 1;
+            expected = new int[0];
+
+            Utilities.Array.Remove(ref array, item);
+            Assert.AreEqual(expected, array);
+        }
+
+        [Test]
+        public void RemoveAtTest()
+        {
+            var array = new[] { 1, 2, 3, 4, 5 };
+            var index = 2;
+            var expected = new[] { 1, 2, 4, 5 };
+
+            Utilities.Array.RemoveAt(ref array, index);
+            Assert.AreEqual(expected, array);
+
+            array = new[] { 1, 2, 3, 4, 5 };
+            index = 4;
+            expected = new[] { 1, 2, 3, 4 };
+
+            Utilities.Array.RemoveAt(ref array, index);
+            Assert.AreEqual(expected, array);
+
+            array = new[] { 1, 2, 3, 4, 5 };
+            index = 0;
+            expected = new[] { 2, 3, 4, 5 };
+
+            Utilities.Array.RemoveAt(ref array, index);
+            Assert.AreEqual(expected, array);
+
+            array = new[] { 1 };
+            index = 0;
+            expected = new int[0];
+
+            Utilities.Array.RemoveAt(ref array, index);
+            Assert.AreEqual(expected, array);
+        }
+
+        [Test]
+        public void RemoveAllTest()
+        {
+            var array = new[] { 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 1, 2, 3, 4, 5 };
+            var item = 3;
+            var expected = new[] { 1, 2, 4, 5, 1, 2, 4, 5, 1, 2, 4, 5 };
+
+            Utilities.Array.RemoveAll(ref array, item);
+            Assert.AreEqual(expected, array);
+
+            array = new[] { 1, 2, 3, 4, 5, 5, 5 };
+            item = 5;
+            expected = new[] { 1, 2, 3, 4 };
+
+            Utilities.Array.RemoveAll(ref array, item);
+            Assert.AreEqual(expected, array);
+
+            array = new[] { 1, 1, 2, 3, 4, 5 };
+            item = 1;
+            expected = new[] { 2, 3, 4, 5 };
+
+            Utilities.Array.RemoveAll(ref array, item);
+            Assert.AreEqual(expected, array);
+
+            array = new[] { 1, 1, 1 };
+            item = 1;
+            expected = new int[0];
+
+            Utilities.Array.RemoveAll(ref array, item);
+            Assert.AreEqual(expected, array);
+        }
+
+        [Test]
+        public void Insert()
+        {
+            var array = new[] { 1, 2, 4, 5 };
+            var index = 2;
+            var item = 3;
+            var expected = new[] { 1, 2, 3, 4, 5 };
+
+            Utilities.Array.Insert(ref array, index, item);
+            Assert.AreEqual(expected, array);
+
+
+            array = new[] { 2, 3, 4, 5 };
+            index = 0;
+            item = 1;
+            expected = new[] { 1, 2, 3, 4, 5 };
+
+            Utilities.Array.Insert(ref array, index, item);
+            Assert.AreEqual(expected, array);
+
+
+            array = new[] { 1, 2, 3, 4 };
+            index = 4;
+            item = 5;
+            expected = new[] { 1, 2, 3, 4, 5 };
+
+            Utilities.Array.Insert(ref array, index, item);
+            Assert.AreEqual(expected, array);
+
+            array = new int[0];
+            index = 0;
+            item = 1;
+            expected = new[] { 1 };
+
+            Utilities.Array.Insert(ref array, index, item);
+            Assert.AreEqual(expected, array);
+        }
+
+        [Test]
+        public void ArrayReferenceEqualsTest()
+        {
+            var array = new[] { "abc", "def", "ghi" };
+            var array2 = new[] { "abc", "def", "ghi" };
+            var expected = true;
+
+            var actual = Utilities.Array.ArrayReferenceEquals(array, array2);
+            Assert.AreEqual(expected, actual);
+
+            array = new[] { "abc", "def", "ghi" };
+            var string1 = new string(new[] { 'a', 'b', 'c' });
+            var string2 = new string(new[] { 'd', 'e', 'f' });
+            var string3 = new string(new[] { 'g', 'h', 'i' });
+            array2 = new[] { string1, string2, string3 };
+            expected = false;
+
+            actual = Utilities.Array.ArrayReferenceEquals(array, array2);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void ContainsTest()
+        {
+            var array = new[] { 1, 2, 3, 4, 5 };
+            var item = 3;
+            var expected = true;
+
+            var actual = Utilities.Array.Contains(array, item);
+            Assert.AreEqual(expected, actual);
+
+            array = new[] { 1, 2, 3, 4, 5 };
+            item = 5;
+            expected = true;
+
+            actual = Utilities.Array.Contains(array, item);
+            Assert.AreEqual(expected, actual);
+
+            array = new[] { 1, 2, 3, 4, 5 };
+            item = 6;
+            expected = false;
+
+            actual = Utilities.Array.Contains(array, item);
+            Assert.AreEqual(expected, actual);
+
+            array = new[] { 1 };
+            item = 1;
+            expected = true;
+
+            actual = Utilities.Array.Contains(array, item);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
