8702971d74355340160753ed45ac4b5df084563c
sergey-cs <prozerox2@yandex.ru>
2020-02-06 15:47:31 +0300

    remove Time field from Utilities Aggregator

--- Assets/Source/Utilities.cs
+++ Assets/Source/Utilities.cs
@@ -7,14 +7,12 @@ namespace Omega.Package
         private static TransformUtilities _transfrom = new TransformUtilities();
         private static GameObjectUtilities _gameObject = new GameObjectUtilities();
         private static RectTransformUtilities _rectTransform = new RectTransformUtilities();
-        private static TimeUtilities _time = new TimeUtilities();
         private static ObjectUtilities _object = new ObjectUtilities();
         private static BoxColliderUtilities _boxCollider = new BoxColliderUtilities();
 
         public static TransformUtilities Transfrom => _transfrom;
         public static GameObjectUtilities GameObject => _gameObject;
         public static RectTransformUtilities RectTransform => _rectTransform;
-        public static TimeUtilities Time => _time;
         public static ObjectUtilities Object => _object;
         public static BoxColliderUtilities BoxCollider => _boxCollider;
     }
