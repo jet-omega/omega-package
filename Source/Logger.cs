1e352fb0d4955b9608fefbd6c53c1d035adadb43
sergey-cs <prozerox2@yandex.ru>
2020-04-03 22:23:50 +0300

    small improve logger

--- Source/Logger.cs
+++ Source/Logger.cs
@@ -7,27 +7,51 @@ namespace Omega.Package
     {
         private RichTextFactory _richTextFactory;
 
-        public string Title;
-        public Color TitleColor;
-        public FontStyle TitleStyle;
+        public readonly string Title;
+        public readonly Color TitleColor;
+        public readonly FontStyle TitleStyle;
+        private readonly string TitleBacked;
 
         public Logger(string title, Color titleColor, FontStyle titleStyle = FontStyle.Normal)
         {
-            _richTextFactory = new RichTextFactory(2048);
             Title = title;
             TitleColor = titleColor;
             TitleStyle = titleStyle;
+
+            _richTextFactory = new RichTextFactory(2048);
+            TitleBacked = _richTextFactory
+                .Style(TitleStyle).Color(TitleColor).Text(Title)
+                .ToString(true);
         }
+
+        private Logger(string prefix, string title, Color titleColor, FontStyle titleStyle = FontStyle.Normal)
+        {
+            Title = title;
+            TitleColor = titleColor;
+            TitleStyle = titleStyle;
+
+            _richTextFactory = new RichTextFactory(2048);
+            TitleBacked = _richTextFactory
+                .UnstyledText(prefix)
+                .Style(TitleStyle).Color(TitleColor).Text(Title)
+                .ToString(true);
+        }
+
 
         public void Log(string message, LogType logType = LogType.Log)
         {
             var fullMessage = _richTextFactory
-                .Style(TitleStyle).Color(TitleColor).Text(Title)
+                .UnstyledText(TitleBacked)
                 .UnstyledText(message).ToString();
-            
+
             _richTextFactory.Clear();
 
             Debug.unityLogger.Log(logType, fullMessage);
+        }
+
+        public Logger CreateSubLogger(string subtitle, Color subtitleColor, FontStyle subtitleStyle = FontStyle.Normal)
+        {
+            return new Logger(TitleBacked, subtitle, subtitleColor, subtitleStyle);
         }
     }
 }
