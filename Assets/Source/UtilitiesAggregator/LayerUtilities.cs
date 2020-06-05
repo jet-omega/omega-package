e5697db2812719230a8133c0687789e4239c5abc
David Nightingale <nightrider2552@gmail.com>
2020-06-05 18:56:22 +0300

    layer utility

---
+++ Assets/Source/UtilitiesAggregator/LayerUtilities.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+
+namespace Omega.Package.Internal
+{
+    public class LayerUtilities
+    {
+        public void LayersInMask(LayerMask layerMask, List<int> layers)
+        {
+            if (layers == null)
+                layers = new List<int>(16);
+            
+            var mask = layerMask.value;
+            for (int i = 0; i < 8 * sizeof(int); mask >>= 1, i++)
+                if ((mask & 1) == 1)
+                    layers.Add(i);
+        }
+    }
+}
