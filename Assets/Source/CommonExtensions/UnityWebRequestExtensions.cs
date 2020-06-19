89e189d95f2801e7dd6aa9419bf3c307be9c1c5b
denisbut1212 <denisbut1212@gmail.com>
2020-06-19 14:16:04 +0300

    asRoutine extension method

---
+++ Assets/Source/CommonExtensions/UnityWebRequestExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Omega.Routines.Web;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace Omega.Package
+{
+    public static class UnityWebRequestExtensions
+    {
+        public static WebRequestRoutine AsRoutine(this UnityWebRequest request)
+        {
+            if (request is null)
+                throw new NullReferenceException(nameof(request));
+
+            return new WebRequestRoutine(request);
+        }
+    }
+}
