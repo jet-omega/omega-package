38fc06292453eaebf82c5e978cccb7f8fda9b965
David Nightingale <nightrider2552@gmail.com>
2020-06-26 17:27:20 +0300

    fix test

--- Assets/Tests/Runtime/Routines/AnyRoutineTests.cs
+++ Assets/Tests/Runtime/Routines/AnyRoutineTests.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using NUnit.Framework;
+using UnityEngine;
 using UnityEngine.TestTools;
 
 namespace Omega.Routines.Tests
@@ -20,16 +21,19 @@ namespace Omega.Routines.Tests
         [UnityTest]
         public IEnumerator AnyRoutineProgressEqualsMaxRoutineProgress()
         {
-            float delay50MsRoutineProgress = 0, anyRoutineProgress = 0;
-            var delay50MsRoutine = Routine.Delay(0.05f)
-                .OnProgress(p => delay50MsRoutineProgress = p);
+            float delay50MsRoutineProgress, delay100MsRoutineProgress, anyRoutineProgress;
+            var delay50MsRoutine = Routine.Delay(0.05f);
             var delay100MsRoutine = Routine.Delay(0.1f);
 
             yield return Routine.Any(delay50MsRoutine, delay100MsRoutine)
                 .OnProgress(p =>
                 {
                     anyRoutineProgress = p;
-                    Assert.AreEqual(delay50MsRoutineProgress, anyRoutineProgress);
+                    delay50MsRoutineProgress = delay50MsRoutine.GetProgress();
+                    delay100MsRoutineProgress = delay100MsRoutine.GetProgress();
+                    Assert.AreEqual(
+                        Mathf.Max(delay50MsRoutineProgress, delay100MsRoutineProgress),
+                        anyRoutineProgress);
                 });
         }
     }
