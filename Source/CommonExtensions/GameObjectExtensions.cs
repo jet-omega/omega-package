62933a4942aca659e99663daaedd7b5af4ad9b2a
David Nightingale <nightrider2552@gmail.com>
2020-07-03 13:35:04 +0300

    add trygetcompoenentinchildren and -parent

--- Source/CommonExtensions/GameObjectExtensions.cs
+++ Source/CommonExtensions/GameObjectExtensions.cs
@@ -59,6 +59,86 @@ public static class GameObjectExtensions
     }
 
     /// <summary>
+    /// TryGetComponent that searches component in children
+    /// </summary>
+    /// <param name="gameObject">GameObject search started from</param>
+    /// <param name="component">Result component</param>
+    /// <typeparam name="T">Component type</typeparam>
+    /// <returns>True if found</returns>
+    /// <exception cref="NullReferenceException">GameObject is null</exception>
+    /// <exception cref="MissingReferenceException">GameObject was destroyed</exception>
+    public static bool TryGetComponentInChildren<T>([NotNull] this GameObject gameObject, out T component) where T : Component
+    {
+        if (gameObject is null)
+            throw new NullReferenceException(nameof(gameObject));
+        if (!gameObject)
+            throw new MissingReferenceException(nameof(gameObject));
+        
+        component = gameObject.GetComponentInChildren<T>();
+        return component != null;
+    }
+    
+    /// <summary>
+    /// TryGetComponent that searches component in children
+    /// </summary>
+    /// <param name="gameObject">GameObject search started from</param>
+    /// <param name="componentType">Component type-object</param>
+    /// <param name="component">Result component</param>
+    /// <returns></returns>
+    /// <exception cref="NullReferenceException">GameObject is null</exception>
+    /// <exception cref="MissingReferenceException">GameObject was destroyed</exception>
+    public static bool TryGetComponentInChildren([NotNull] this GameObject gameObject, Type componentType, out Component component)
+    {
+        if (gameObject is null)
+            throw new NullReferenceException(nameof(gameObject));
+        if (!gameObject)
+            throw new MissingReferenceException(nameof(gameObject));
+        
+        component = gameObject.GetComponentInChildren(componentType);
+        return component != null;
+    }
+
+    /// <summary>
+    /// TryGetComponent that searches component in parents
+    /// </summary>
+    /// <param name="gameObject">GameObject search started from</param>
+    /// <param name="component">Result component</param>
+    /// <typeparam name="T">Component type</typeparam>
+    /// <returns>True if found</returns>
+    /// <exception cref="NullReferenceException">GameObject is null</exception>
+    /// <exception cref="MissingReferenceException">GameObject was destroyed</exception>
+    public static bool TryGetComponentInParent<T>([NotNull] this GameObject gameObject, out T component) where T : Component
+    {
+        if (gameObject is null)
+            throw new NullReferenceException(nameof(gameObject));
+        if (!gameObject)
+            throw new MissingReferenceException(nameof(gameObject));
+        
+        component = gameObject.GetComponentInParent<T>();
+        return component != null;
+    }
+    
+    /// <summary>
+    /// TryGetComponent that searches component in parents
+    /// </summary>
+    /// <param name="gameObject">GameObject search started from</param>
+    /// <param name="componentType">Component type-object</param>
+    /// <param name="component">Result component</param>
+    /// <returns></returns>
+    /// <exception cref="NullReferenceException">GameObject is null</exception>
+    /// <exception cref="MissingReferenceException">GameObject was destroyed</exception>
+    public static bool TryGetComponentInParent([NotNull] this GameObject gameObject, Type componentType, out Component component)
+    {
+        if (gameObject is null)
+            throw new NullReferenceException(nameof(gameObject));
+        if (!gameObject)
+            throw new MissingReferenceException(nameof(gameObject));
+        
+        component = gameObject.GetComponentInParent(componentType);
+        return component != null;
+    }
+
+    /// <summary>
     /// Возвращает всех потомков для игрового объекта
     /// </summary>
     /// 
