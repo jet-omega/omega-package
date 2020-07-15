3f02456878162c0fe9ddc206a777a66a1322fd8a
David Nightingale <nightrider2552@gmail.com>
2020-07-15 11:32:42 +0300

    fix stacktraceutility

--- Assets/Source/Package/StackTraceUtility.cs
+++ Assets/Source/Package/StackTraceUtility.cs
@@ -11,76 +11,7 @@ namespace Omega.Package
     /// </summary>
     internal static class StackTraceUtility
     {
-        private static string _projectFolder = "";
-
-        internal static string ExtractStringFromException(object exception)
-        {
-            ExtractStringFromException(exception, out var message, out var stackTrace);
-            return message + "\n" + stackTrace;
-        }
-
-        private static void SetupProjectFolder()
-        {
-            _projectFolder = Application.dataPath;
-        }
-
-        private static void ExtractStringFromException(
-            object exceptionObject,
-            out string message,
-            out string stackTrace)
-        {
-            if (exceptionObject == null)
-                throw new ArgumentException("ExtractStringFromExceptionInternal called with null exception");
-            if (!(exceptionObject is Exception exception))
-                throw new ArgumentException(
-                    "ExtractStringFromExceptionInternal called with an exception that was not of type System.Exception");
-            var stringBuilder = new StringBuilder(exception.StackTrace?.Length * 2 ?? 512);
-            message = "";
-            var str1 = "";
-            for (; exception != null; exception = exception.InnerException)
-            {
-                if(exception.StackTrace is null)
-                    break;
-
-                str1 = str1.Length != 0 ? exception.StackTrace + "\n" + str1 : exception.StackTrace;
-                
-                var regex = new Regex(" at ");
-                // ReSharper disable once AssignNullToNotNullAttribute
-                foreach (Match match in regex.Matches(str1))
-                    str1 = str1.Replace(match.Value, "");
-                
-                regex = new Regex("\\S:.*Assets");
-                foreach (Match match in regex.Matches(str1))
-                    str1 = str1.Replace(match.Value, "(at Assets");
-                
-                regex = new Regex(" \\[0x\\S*\\] in ");
-                foreach (Match match in regex.Matches(str1))
-                    str1 = str1.Replace(match.Value, " ");
-                
-                regex = new Regex("cs:\\d* ");
-                var matches = regex.Matches(str1);
-                foreach (Match match in matches)
-                    str1 = str1.Replace(match.Value, match.Value.Replace(" ", ")"));
-                
-                str1 = str1.Replace("\\", "/");
-
-
-                var str2 = exception.GetType().Name;
-                var str3 = "";
-                if (exception.Message != null)
-                    str3 = exception.Message;
-                if (str3.Trim().Length != 0)
-                    str2 = str2 + ": " + str3;
-                message = str2;
-                if (exception.InnerException != null)
-                    str1 = "Rethrow as " + str2 + "\n" + str1;
-            }
-
-            stringBuilder.Append(str1 + "\n");
-            var stackTrace1 = new StackTrace(1, true);
-            stringBuilder.Append(ExtractFormattedStackTrace(stackTrace1));
-            stackTrace = stringBuilder.ToString();
-        }
+        private static string _projectFolder = string.Empty;
 
         internal static string ExtractFormattedStackTrace(StackTrace stackTrace)
         {
@@ -109,13 +40,13 @@ namespace Omega.Package
                 var index2 = 0;
                 var parameters = method.GetParameters();
                 var flag = true;
-                for (; index2 < parameters.Length; ++index2)
+                while (index2 < parameters.Length)
                 {
                     if (!flag)
                         stringBuilder.Append(", ");
                     else
                         flag = false;
-                    stringBuilder.Append(parameters[index2].ParameterType.Name);
+                    stringBuilder.Append(parameters[index2++].ParameterType.Name);
                 }
 
                 stringBuilder.Append(")");
@@ -130,10 +61,12 @@ namespace Omega.Package
                      declaringType.Namespace != "UnityEngine"))
                 {
                     stringBuilder.Append(" (at ");
-                    if (!string.IsNullOrEmpty(_projectFolder) &&
-                        str2.Replace("\\", "/").StartsWith(_projectFolder))
-                        str2 = str2.Substring(_projectFolder.Length,
-                            str2.Length - _projectFolder.Length);
+                    if (!string.IsNullOrEmpty(_projectFolder) && str2.Replace("\\", "/").StartsWith(_projectFolder))
+                    {
+                        stringBuilder.Append("Assets");
+                        str2 = str2.Substring(_projectFolder.Length, str2.Length - _projectFolder.Length);
+                    }
+
                     stringBuilder.Append(str2);
                     stringBuilder.Append(":");
                     stringBuilder.Append(frame.GetFileLineNumber().ToString());
@@ -144,6 +77,75 @@ namespace Omega.Package
             }
 
             return stringBuilder.ToString().Replace("\\", "/");
+        }
+
+        internal static string ExtractStringFromException(object exception)
+        {
+            ExtractStringFromException(exception, out var message, out var stackTrace);
+            return message + "\n" + stackTrace;
+        }
+
+        private static void SetupProjectFolder()
+        {
+            _projectFolder = Application.dataPath;
+        }
+
+        private static void ExtractStringFromException(object exceptionObject, out string message,
+            out string stackTrace)
+        {
+            if (exceptionObject == null)
+                throw new ArgumentException("ExtractStringFromExceptionInternal called with null exception");
+            if (!(exceptionObject is Exception exception))
+                throw new ArgumentException(
+                    "ExtractStringFromExceptionInternal called with an exception that was not of type System.Exception");
+            var stringBuilder = new StringBuilder(exception.StackTrace?.Length * 2 ?? 512);
+            message = string.Empty;
+            var str1 = string.Empty;
+            
+            while (exception != null)
+            {
+                if (exception.StackTrace is null)
+                    break;
+
+                str1 = str1.Length != 0 ? exception.StackTrace + "\n" + str1 : exception.StackTrace;
+
+                var regex = new Regex(" at ");
+                // ReSharper disable once AssignNullToNotNullAttribute
+                foreach (Match match in regex.Matches(str1))
+                    str1 = str1.Replace(match.Value, "");
+
+                regex = new Regex("\\S:.*Assets");
+                foreach (Match match in regex.Matches(str1))
+                    str1 = str1.Replace(match.Value, "(at Assets");
+
+                regex = new Regex(" \\[0x\\S*\\] in ");
+                foreach (Match match in regex.Matches(str1))
+                    str1 = str1.Replace(match.Value, " ");
+
+                regex = new Regex("cs:\\d* ");
+                var matches = regex.Matches(str1);
+                foreach (Match match in matches)
+                    str1 = str1.Replace(match.Value, match.Value.Replace(" ", ")"));
+
+                str1 = str1.Replace("\\", "/");
+
+                var str2 = exception.GetType().Name;
+                var str3 = "";
+                if (exception.Message != null)
+                    str3 = exception.Message;
+                if (str3.Trim().Length != 0)
+                    str2 = str2 + ": " + str3;
+                message = str2;
+                if (exception.InnerException != null)
+                    str1 = "Rethrow as " + str2 + "\n" + str1;
+
+                exception = exception.InnerException;
+            }
+
+            stringBuilder.Append(str1 + "\n");
+            var stackTrace1 = new StackTrace(1, true);
+            stringBuilder.Append(ExtractFormattedStackTrace(stackTrace1));
+            stackTrace = stringBuilder.ToString();
         }
     }
 }
