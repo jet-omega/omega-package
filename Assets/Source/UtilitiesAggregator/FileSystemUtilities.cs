79f61de2be5c0bd632b2f4f01d1e0247e2995854
David Nightingale <nightrider2552@gmail.com>
2020-07-16 11:08:38 +0300

    add file system utilities

---
+++ Assets/Source/UtilitiesAggregator/FileSystemUtilities.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Omega.Package.Internal
+{
+    public class FileSystemUtilities
+    {
+        public DirectoryInfo MissingDirectoryPath(string fullPath)
+        {
+            fullPath = fullPath.Replace('/', '\\');
+
+            if (Directory.Exists(fullPath))
+                return new DirectoryInfo(fullPath);
+
+            var root = fullPath.Substring(0, 3);
+            var rootRegex = new Regex(@"[A-z]:\\");
+            if (!rootRegex.IsMatch(root))
+                throw new ArgumentException("Root not found. Path should start with \"x:/\" where 'x' is volume", nameof(fullPath));
+                
+            if (!Directory.Exists(root))
+                throw new ArgumentException($"There is no volume {root} found on this machine");
+
+            var directories = fullPath.Split('\\');
+
+            var currentPath = $@"{directories[0]}\";
+
+            for (int i = 1; i < directories.Length; i++)
+            {
+                currentPath = Path.Combine(currentPath, directories[i]);
+                if (!Directory.Exists(currentPath))
+                    Directory.CreateDirectory(currentPath);
+            }
+
+            return new DirectoryInfo(currentPath);
+        }
+    }
+}
---
+++ Assets/Tests/Editor/FileSystemUtilitiesTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+using Omega.Package;
+using UnityEngine;
+
+namespace Omega.Tools.Tests
+{
+    public class FileSystemUtilitiesTests
+    {
+        [Test]
+        public void CorrectPathShouldReturnDirectoryInfo()
+        {
+            var path = Application.dataPath;
+            var info = Utilities.FileSystem.MissingDirectoryPath(path);
+            Assert.AreEqual("Assets", info.Name);
+        }
+
+        [Test]
+        public void UnknownVolumeShouldThrowException()
+        {
+            var driveInfos = DriveInfo.GetDrives();
+            var unknownVolumeLetterCode = (int) 'A';
+            while (driveInfos.Any(t => t.Name[0] == (char) unknownVolumeLetterCode))
+                unknownVolumeLetterCode++;
+            var path = (char) unknownVolumeLetterCode + @":\";
+            Assert.Catch<ArgumentException>(() => Utilities.FileSystem.MissingDirectoryPath(path));
+        }
+
+        [Test]
+        public void PartialPathShouldReturnNewDirectoryInfo()
+        {
+            var directoryName = DateTime.Now.Ticks.ToString();
+            var path = Path.Combine(Application.dataPath, directoryName);
+            var info = Utilities.FileSystem.MissingDirectoryPath(path);
+            Assert.AreEqual(directoryName, info.Name);
+            Directory.Delete(path);
+        }
+    }
+}
